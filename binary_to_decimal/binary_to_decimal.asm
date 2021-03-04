457ab65c3eb9ec7bb975ddf78ddeaafcedce68fa
Noah Abe <noahabe64@gmail.com>
2021-03-04 14:04:32 +0300

    added a gitignore and a makefile

--- binary_to_decimal/binary_to_decimal.asm
+++ binary_to_decimal/binary_to_decimal.asm
@@ -5,13 +5,18 @@ extern printf
 section .data
 	thebinarynumber db "1011110111",0
 	thedecimalnumber dq 0
-	msg db "Binary: %s\t Decimal: %d\n",0
+	msg db "Binary: %s",9,"Decimal: %d",10,0
 section .bss
 	result resq 1
 section .text
 	global main
 main:
-	
-	
+;convert the binary number to the decimal number
+;by the common algorithm
+		
 ;display the result
-			
+	mov rdi,msg
+	mov rsi,thebinarynumber
+	mov rdx,[thedecimalnumber]
+	mov rax,0
+	call printf			
