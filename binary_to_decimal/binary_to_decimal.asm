44e14128f1683842d03eaaac27f679424d511de5
Noah Abe <noahabe64@gmail.com>
2021-03-04 14:47:55 +0300

    finished first round

--- binary_to_decimal/binary_to_decimal.asm
+++ binary_to_decimal/binary_to_decimal.asm
@@ -4,6 +4,7 @@
 extern printf
 section .data
 	thebinarynumber db "1011110111",0
+	exponent equ $-thebinarynumber-2  
 	thedecimalnumber dq 0
 	msg db "Binary: %s",9,"Decimal: %d",10,0 ;9 is ascii of \t 
 						 ;10 is ascii of \n
@@ -12,10 +13,32 @@ section .bss
 section .text
 	global main
 main:
-
+	xor rcx,rcx
 ;convert the binary number to the decimal number
 ;by the common algorithm
-			
+	mov rcx, exponent
+	xor rax,rax
+	xor rbx,rbx
+	mov rbx,1
+
+	mov rdx,thebinarynumber
+	add rdx,exponent
+	add rdx,1
+
+	xor r8,r8
+
+bloop:
+	mov rbx,1
+	mov rax,[rdx]
+	sub rax,48
+	imul qword rbx
+	add r8,rax
+	dec rdx
+	add rbx,rbx
+	loop bloop
+	
+	mov [thedecimalnumber],r8
+				
 ;display the result
 	mov rdi,msg
 	mov rsi,thebinarynumber
